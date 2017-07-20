8cd2f0b576bbb02eb5e96e4b9150cda4cce41e0b
Diego Keller <keller.diego@gmail.com>
2017-07-20 16:16:42 -0300

    .

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return [
+        {title: 'Livro 1', pages: 101},
+        {title: 'Livro 2', pages: 102},
+        {title: 'Livro 3', pages: 103},
+        {title: 'Livro 4', pages: 104},
+        {title: 'Livro 5', pages: 105}
+    ];
+}
